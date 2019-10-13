380d8d523e0c60f68d2a8a0f5e079f22cd9223ca
amateur-dev <>
2019-10-13 13:39:21 +0800

    Upgraded Main Contract
    This contract should hold the DAI for converting it to cDAI

--- permitting_compound.sol
+++ permitting_compound.sol
@@ -1,21 +1,54 @@
-pragma solidity ^0.4.18;
+pragma solidity >0.4.0 <0.6.0;
 
-import "./DAIStablecoin.sol";
+interface Daistablecoin {
+   function approve(address guy, uint wad) external returns (bool);
+}
+
+
+pragma solidity ^0.5.0;
+
+import "./CompoundDai.sol";
 
 contract permitting_compound {
-    DSToken public DSTokenContract;
+    CErc20 public CErc20Contract;
+    Daistablecoin public DSTokenContract;
+    uint public balance = address(this).balance;
     
-    event Approval(address indexed src, address indexed guy, uint wad);
+    // function permitting_compound (
+    //     DSToken _DSTokenContract,
+    //     CErc20 _CErc20Contract
+    // ) public {
+    //     DSTokenContract = _DSTokenContract;
+    //     CErc20Contract = _CErc20Contract;
+    // }
     
-    function permitting_compound (
-        DSToken _DSTokenContract
-    ) public {
-        DSTokenContract = _DSTokenContract;
+    constructor (CErc20 _CErc20Contract) public {
+        CErc20Contract = _CErc20Contract;
     }
+    
+    function setDaiStableCoinAddress(Daistablecoin _DSTokenContract) public {
+        DSTokenContract = _DSTokenContract;
+        
+    }
+    
+    function depositETH() payable public returns (uint) {
+        balance += msg.value;
+    }
+    
+    // fallback function let you / anyone send ETH to this wallet without the need to call any function
+    function() external payable {
+        balance += msg.value;
+    }
+    
     
     function approveSomeone(address _whomtoApprove, uint256 _howmuchtoApprove) public returns (bool) {
         bool result = DSTokenContract.approve(_whomtoApprove, _howmuchtoApprove);
         return result;
     }
-  
+    
+    function doSomeMinting(uint256 _howmuchtoMint) public {
+        CErc20Contract.mint(_howmuchtoMint);
+    }
+    
+    
 }
