295a85f565a84a950111d5fde10d35200e5437b1
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-10-17 07:39:01 +0530

    Starting up our code base for Aggresive ETH Savings.

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HomePage from './components/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
