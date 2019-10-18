3bfd0fb9ec6a2f2ce89790fd0cd0e0e1c68b3b4e
roheat <rohithsrivathsav@gmail.com>
2019-10-19 02:07:46 +0530

    dashbaord and charts added

--- website/src/App.js
+++ website/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import HomePage from "./components/HomePage";
 import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Dashboard />
     </div>
   );
 }
