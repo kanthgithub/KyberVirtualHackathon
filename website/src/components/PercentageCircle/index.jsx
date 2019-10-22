26d9e1f5d828c5bc887319ff776ae4c292bc67dd
roheat <rohithsrivathsav@gmail.com>
2019-10-23 01:36:14 +0530

    added baskets page

--- website/src/components/PercentageCircle/index.jsx
+++ website/src/components/PercentageCircle/index.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import "../../App.css";
 
 const DEFAULT_VALUE = 0;
-const DEFAULT_CDAI = "#5540BF";
+const DEFAULT_COLOR = "#5540BF";
 const DEFAULT_MINVOL = "#F14A5C";
 const DEFAULT_SMA20 = "#11112F";
 const DEFAULT_MAX_VALUE = 100;
 
 export default props => {
-  const cDAI = props.cDAI || DEFAULT_VALUE;
-  const minvol = props.minvol || DEFAULT_VALUE;
-  const sma20 = props.sma20 || DEFAULT_VALUE;
+  const percentage = props.percentage || DEFAULT_VALUE;
+  const color = props.color || DEFAULT_COLOR;
+  // const minvol = props.minvol || DEFAULT_VALUE;
+  // const sma20 = props.sma20 || DEFAULT_VALUE;
   const maxValue = props.maxValue || DEFAULT_MAX_VALUE;
 
   const radius = 90;
   const dashArray = radius * Math.PI * 2;
-  const dashOffsetCDAI = dashArray - dashArray * (cDAI / maxValue);
-  const dashOffsetMINVOL =
-    dashArray - dashArray * (minvol / maxValue) - dashArray * (cDAI / maxValue);
-  const dashOffsetSMA20 =
-    dashArray -
-    dashArray * (minvol / maxValue) -
-    dashArray * (cDAI / maxValue) -
-    dashArray * (sma20 / maxValue);
+  const dashOffset = dashArray - dashArray * (percentage / maxValue);
+  // const dashOffsetMINVOL =
+  //   dashArray - dashArray * (minvol / maxValue) - dashArray * (cDAI / maxValue);
+  // const dashOffsetSMA20 =
+  //   dashArray -
+  //   dashArray * (minvol / maxValue) -
+  //   dashArray * (cDAI / maxValue) -
+  //   dashArray * (sma20 / maxValue);
   return (
-    <div>
+    <div className="circleContainer mx-3">
       <svg viewBox="0 0 200 200">
         <circle
           className="circleBackground"
@@ -33,7 +34,7 @@ export default props => {
           r={radius}
           strokeWidth="10px"
         />
-        <circle
+        {/* <circle
           className="circleProgress"
           cx="100"
           cy="100"
@@ -58,7 +59,7 @@ export default props => {
             strokeDashoffset: dashOffsetMINVOL,
             stroke: DEFAULT_MINVOL
           }}
-        />
+        /> */}
         <circle
           className="circleProgress"
           cx="100"
@@ -68,22 +69,16 @@ export default props => {
           transform="rotate(-90 100 100)"
           style={{
             strokeDasharray: dashArray,
-            strokeDashoffset: dashOffsetCDAI,
-            stroke: DEFAULT_CDAI
+            strokeDashoffset: dashOffset,
+            stroke: color
           }}
         />
-
-        {/*<text
-          className="circleText"
-          x="50%"
-          y="50%"
-          dy=".3em"
-          textAnchor="middle"
-        >
-          {props.children}
-        </text>*/}
-
-        {props.children}
+        <foreignObject x="30%" y="25%" width="150" height="100">
+          <p xmlns="http://www.w3.org/1999/xhtml" className="circleText d-flex">
+            {percentage}
+            <span className="percentageText mt-4">%</span>
+          </p>
+        </foreignObject>
       </svg>
     </div>
   );
--- website/src/pages/baskets/index.js
+++ website/src/pages/baskets/index.js
@@ -1,5 +1,308 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PercentageCircle from "../../components/PercentageCircle";
 
-const Baskets = () => <h1>Baskets</h1>;
+const Baskets = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container">
+          <a className="navbar-brand">
+            <h2>DeFi Strategies</h2>
+          </a>
+        </div>
+      </nav>
+      <section className="pt-5 pb-5 pt-md-12">
+        <div className="container">
+          <h1 className="display-3 text-center">Baskets</h1>
+          <p className="text-center text-muted col-12">
+            Allocate your money across{" "}
+            <span className="text-dark">
+              Compound + TokenSets + Fulcrum + Synthetix + dYdX
+            </span>{" "}
+            <br />
+            in one single transaction.
+          </p>
+        </div>
+      </section>
 
+      {/* Hedger Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 hedger">Hedger</h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 10% in cDAI, 20% in ETHHIVOL, 20% in ETH12EMA, 20% in
+                    ETH26EMA, 5% in LONG2xBZX and 5% in SHORTdYdX.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={10} color="#5540bf" />
+                    <h6 className="text-center mt-3">cDAI</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={20} color="#5540bf" />
+                    <h6 className="text-center mt-3">ETHHIVOL</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={20} color="#5540bf" />
+                    <h6 className="text-center mt-3">ETH20SMACO</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={20} color="#5540bf" />
+                    <h6 className="text-center mt-3">ETH12EMA</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={20} color="#5540bf" />
+                    <h6 className="text-center mt-3">ETH26EMA</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={"05"} color="#5540bf" />
+                    <h6 className="text-center mt-3">LONG2xBZX</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={"05"} color="#5540bf" />
+                    <h6 className="text-center mt-3">SHORTdYdX</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-center my-4">
+                <div className="col-12 col-md-12 col-lg-12 text-center ">
+                  <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                    Buy Basket
+                  </button>
+                </div>
+                <div className="my-4">
+                  <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                    View Analytics
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Short-term Bear Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 shortTermBear">
+                    Short-term Bear
+                  </h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 50% in cDAI, 30% in ETHMINVOL and 20% in SHORTdYdX.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={50} color="#f03b68" />
+                    <h6 className="text-center mt-3">cDAI</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={30} color="#f03b68" />
+                    <h6 className="text-center mt-3">ETHMINVOL</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={20} color="#f03b68" />
+                    <h6 className="text-center mt-3">SHORTdYdX</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-12 col-lg-12 text-center ">
+                  <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                    Buy Basket
+                  </button>
+                </div>
+                <div className="my-4">
+                  <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                    View Analytics
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Short-term Swings Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 shortTermSwings">
+                    Short-term Swings{" "}
+                  </h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 30% in cDAI, 35% in ETHHIVOL, 35% in ETHMINVOL.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={30} color="#262626" />
+                    <h6 className="text-center mt-3">cDAI</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={35} color="#262626" />
+                    <h6 className="text-center mt-3">ETHHIVOL</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={35} color="#262626" />
+                    <h6 className="text-center mt-3">ETHMINVOL</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-center my-4">
+                <div className="col-12 col-md-12 col-lg-12 text-center ">
+                  <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                    Buy Basket
+                  </button>
+                </div>
+                <div className="my-4">
+                  <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                    View Analytics
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Short-term Bull Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 shortTermBull">
+                    Short-term Bull
+                  </h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 70% in ETHMINVOL and 30% in LONG2xBZX.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={70} color="#f49124" />
+                    <h6 className="text-center mt-3">ETHMINVOL</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={30} color="#f49124" />
+                    <h6 className="text-center mt-3">LONG2xBZX</h6>
+                  </div>
+                </div>
+                <div className="row justify-content-center my-4">
+                  <div className="col-12 col-md-12 col-lg-12 text-center ">
+                    <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                      Coming Soon
+                    </button>
+                  </div>
+                  <div className="my-4">
+                    <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                      View Analytics
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Long-term Bull Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 longTermBull">
+                    Long-term Bull{" "}
+                  </h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 70% in ETHHIVOL and 30% in LONG2xBZX.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={70} color="#4bc0c0" />
+                    <h6 className="text-center mt-3">ETHHIVOL</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={30} color="#4bc0c0" />
+                    <h6 className="text-center mt-3">LONG2xBZX</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-center my-4">
+                <div className="col-12 col-md-12 col-lg-12 text-center ">
+                  <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                    Buy Basket
+                  </button>
+                </div>
+                <div className="my-4">
+                  <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                    View Analytics
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Lender Basket */}
+      <section className="pb-5 pt-md-12">
+        <div className="container">
+          <div className="card shadow" style={{ backgroundColor: "#ffffff" }}>
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-8">
+                  <h1 className="text-center my-4 lender">Lender</h1>
+                  <p className="my-4 text-center text-muted">
+                    Invest 85% in cDAI and 15% in ETHHIVOL.
+                  </p>
+                </div>
+                <div className="col-12 col-md-6 col-lg-12 justify-content-center d-flex my-4">
+                  <div>
+                    <PercentageCircle percentage={85} color="#1314ad" />
+                    <h6 className="text-center mt-3">cDAI</h6>
+                  </div>
+                  <div>
+                    <PercentageCircle percentage={15} color="#1314ad" />
+                    <h6 className="text-center mt-3">ETHHIVOL</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="row justify-content-center my-4">
+                <div className="col-12 col-md-12 col-lg-12 text-center ">
+                  <button className="font20 btn btn-outline-dark btn-large shadow rounded-pill px-4 py-2 ">
+                    Buy Basket
+                  </button>
+                </div>
+                <div className="my-4">
+                  <button className="btn btn-outline-primary shadow rounded-pill px-4 py-2">
+                    View Analytics
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 export default Baskets;
