79decd655dade06b3c81eed69f39699a23fe5e40
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-10-27 13:56:20 +0530

    Some code refactor and adding a navbar to the baskets page.

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import NavigationBar from "./Navbar";
+import NavigationBar from "../Navbar";
 import "../../App.css";
 import heroimg from "../../assets/hero.svg";
 
--- src/components/PercentageCircle/index.jsx
+++ src/components/PercentageCircle/index.jsx
@@ -1,29 +1,20 @@
 import React from "react";
+
 import "../../App.css";
 
 const DEFAULT_VALUE = 0;
 const DEFAULT_COLOR = "#5540BF";
-// const DEFAULT_MINVOL = "#F14A5C";
-// const DEFAULT_SMA20 = "#11112F";
 const DEFAULT_MAX_VALUE = 100;
 
 export default props => {
   const percentage = props.percentage || DEFAULT_VALUE;
   const color = props.color || DEFAULT_COLOR;
-  // const minvol = props.minvol || DEFAULT_VALUE;
-  // const sma20 = props.sma20 || DEFAULT_VALUE;
   const maxValue = props.maxValue || DEFAULT_MAX_VALUE;
 
   const radius = 90;
   const dashArray = radius * Math.PI * 2;
   const dashOffset = dashArray - dashArray * (percentage / maxValue);
-  // const dashOffsetMINVOL =
-  //   dashArray - dashArray * (minvol / maxValue) - dashArray * (cDAI / maxValue);
-  // const dashOffsetSMA20 =
-  //   dashArray -
-  //   dashArray * (minvol / maxValue) -
-  //   dashArray * (cDAI / maxValue) -
-  //   dashArray * (sma20 / maxValue);
+
   return (
     <div className="circleContainer mx-3">
       <svg viewBox="0 0 200 200">
@@ -34,32 +25,6 @@ export default props => {
           r={radius}
           strokeWidth="10px"
         />
-        {/* <circle
-          className="circleProgress"
-          cx="100"
-          cy="100"
-          r={radius}
-          strokeWidth="15px"
-          transform="rotate(-90 100 100)"
-          style={{
-            strokeDasharray: dashArray,
-            strokeDashoffset: dashOffsetSMA20,
-            stroke: DEFAULT_SMA20
-          }}
-        />
-        <circle
-          className="circleProgress"
-          cx="100"
-          cy="100"
-          r={radius}
-          strokeWidth="15px"
-          transform="rotate(-90 100 100)"
-          style={{
-            strokeDasharray: dashArray,
-            strokeDashoffset: dashOffsetMINVOL,
-            stroke: DEFAULT_MINVOL
-          }}
-        /> */}
         <circle
           className="circleProgress"
           cx="100"
