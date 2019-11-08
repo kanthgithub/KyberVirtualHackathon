39baae48c47698a93fc22fb4cd69af4e43b7df25
amateur-dev <>
2019-11-08 14:42:24 +0800

    Fixed something in SVG

--- src/components/Footer/FooterView.jsx
+++ src/components/Footer/FooterView.jsx
@@ -26,9 +26,6 @@ const FooterView = () => (
         Try on local, the footer currently has a bg-light background so it's not that bad.
         */}
       <Col sm md lg className="p-1 m-1">
-        <img src={tokensets} className="img-fluid" width="150px" alt="tokensets" />
-      </Col>
-      <Col sm md lg className="p-1 m-1">
         <img src={bzx} className="img-fluid" width="150px" alt="bzx" />
       </Col>
       <Col sm md lg className="p-1 m-1">
@@ -36,6 +33,9 @@ const FooterView = () => (
       </Col>
       <Col sm md lg className="p-1 m-1">
         <img src={synthetix} className="img-fluid" width="150px" alt="synthetix" />
+      </Col>
+      <Col sm md lg className="p-1 m-1">
+        <img src={tokensets} className="img-fluid" width="150px" alt="tokensets" />
       </Col>
     </Row>
   </Row>
