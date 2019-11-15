4c975509f6ace7ed1fe18f2abeb240100bd5221a
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-11-15 10:55:30 +0530

    Added uniswap and fixed other stuff.

--- src/components/Footer/FooterView.jsx
+++ src/components/Footer/FooterView.jsx
@@ -8,21 +8,24 @@ import bzx from '../../assets/FooterIcons/bzx.svg';
 import compound from '../../assets/FooterIcons/compound.svg';
 import kyber from '../../assets/FooterIcons/kyber.svg';
 import tokensets from '../../assets/FooterIcons/set-logo-color.svg';
+import uniswap from '../../assets/FooterIcons/uniswap.svg';
 
 const FooterView = () => (
   <>
-    <Row className="font-weight-bold justify-content-center">Built on</Row>
-    <Row className="pt-0 mt-0 text-center justify-content-center mb-4">
-      <Col sm md lg className="p-1 m-1">
-        <img src={ethereum} width="200px" alt="ethereum" />
+    <Row className="justify-content-center px-1 mb-1">
+      <h2>Built On</h2>
+    </Row>
+    <Row className="px-0 mx-0 text-center justify-content-center mb-4">
+      <Col sm md lg className="p-1">
+        <img src={ethereum} width="250px" alt="ethereum" />
       </Col>
-      <Col sm md lg className="p-1 m-1">
+      <Col sm md lg className="p-1">
         <img src={compound} width="200px" alt="compound" />
       </Col>
-      <Col sm md lg className="p-1 m-1">
+      <Col sm md lg className="p-1">
         <img src={bzx} width="120px" alt="bzx" />
       </Col>
-      <Col sm md lg className="p-1 m-1">
+      <Col sm md lg className="p-1">
         <img src={kyber} width="100px" alt="kyber" />
       </Col>
       <Col
@@ -33,7 +36,10 @@ const FooterView = () => (
       >
         <img src={synthetix} width="200px" alt="synthetix" />
       </Col>
-      <Col sm md lg className="p-1 m-1">
+      <Col sm md lg className="p-1">
+        <img src={uniswap} width="60px" alt="uniswap" />
+      </Col>{' '}
+      <Col sm md lg className="p-1">
         <img src={tokensets} width="100px" alt="tokensets" />
       </Col>
     </Row>
